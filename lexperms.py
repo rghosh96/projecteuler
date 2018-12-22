721bc1636ad8a9b23a1506461324d43b4d90f10c
Rashi Ghosh <rghosh@Rashis-MacBook-Pro.local>
2018-12-21 19:41:11 -0600

    a few edits later after a "i messed up the repository" scare

--- lexperms.py
+++ lexperms.py
@@ -1,6 +1,109 @@
 from itertools import permutations
 
-string = [3,1,2,4]
+#like what if this was 2314, the order of the list of permutations would differ, yes but read the question sir
+#okay sure here u can put it in however u want, but if like, a random user were doing this and entered
+#okay nvm i see ur point
+#(laughing crying face emoji) oh my god pls be more okay
 
-string = sorted(string)
-print(list((permutations(string, r=None))))
+ #sort first ? why... it doesn't matter sort doesn't do that
+#yes it does, bc it sorts in lexicographic order, the millionth one entails there's an order
+#permutations lists the combinations in lexicographic order if you feed it in sorted
+#if that makes sense ?
+# print(list((permutations(string))))
+
+a = permutations(range(10))
+for i in range(10**6-1):
+    next(a)
+ans = next(a)
+print(ans) # output = (2, 7, 8, 3, 9, 1, 5, 4, 6, 0)
+a = ''
+for i in ans:
+    a += str(i)
+print(a) # output = 2783915460 #dab
+
+
+
+for i in range(10**6-1):
+    #next(a)
+    break
+
+#sir why omg
+
+b = permutations(range(10))
+count = 0 # count is 0
+for i in range(10**6-1):
+    count += 1
+eeep = list(b)
+print(eeep[count])
+print(str(eeep[count]))
+
+
+# x = count
+# answer = ''
+# for i in range(len(a)):
+#     answer += a[x]
+#     x +=
+
+#k thx i kno u r (laughing crying emoji
+#HELLO !!!!!!
+# don't judge me i'm trying to learn things ): i want to see all the permutations rn
+# lit, sounds great, just use the toggle git tab?
+# um
+# I'm not judging u I'm tryingt to figure out how to get this linked to the repository well yeah but I had to make a folder and stuff
+# hello
+# sorry I'll just watch now
+
+#run it, try running it
+#wtf no!!!! i totally thought that up!!!!! while i was fucking
+#washing the dishes
+#you called the permutations iterable a
+#that's why it's a
+#right bc i didn't iterate thru all 10 places
+#i have to now basically piggy back off of u at this point with
+#the whole string idea
+#i basically used c++ logic
+#no but i can upload it to the repository, so can u right?
+#fuck me.
+#also you realize........ your loop..... does not do anything
+# You just piggybacked on my code hoe yes you did, you didn't mak
+
+#nvm it's like 3 million so idk why this iterator isn't working
+
+#i'm getting an error for next(a)
+#let me try as if it were c++
+#can u undo what you just did lol How's that :)ju OH WE FORGOT 0!!! rip
+# shit my bad lol go ahead
+
+#does that not print out a million numbers ???
+#OH and then i see i see ise e iseeeeee i just noticed the print statement out of the loop
+#i hate how there are not braces to contain things in things
+#c++ is much nicer to read
+
+#q: is next(a) similar in idea to i++ if i = 0 before the loop?
+# I don't know c++ lol
+# next just calls next value of the iterator ;; gotcha i see
+# also we low key cheated by using itertools but it's fine
+# i lo key want to figure out the algorithm but also
+#y;; what??? 10P10 ?
+# I actually didn't mean to do that lolo
+# Dude this fucking itertools thing is wack as shit
+# what even is 10P10???
+# 10 factorial
+#no it calls the next method 999999 times and prints the millionth
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#look sir
+#i was just testing these functions lolol (crying laughing emoji)
